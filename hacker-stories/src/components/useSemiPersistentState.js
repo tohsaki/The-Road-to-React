f06fab66f815de73f888c8039c82e3e7c690aac9
Tsuni <tohsaki@cox.net>
2021-08-27 16:46:47 -0700

    Don't run on first render

--- hacker-stories/src/components/useSemiPersistentState.js
+++ hacker-stories/src/components/useSemiPersistentState.js
@@ -1,9 +1,17 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const useSemiPersistentState = (key, initialState) => {
+  const isMounter = React.useRef(false);
+
   const [value, setValue] = useState(localStorage.getItem(key) || initialState);
+
   useEffect(() => {
-    localStorage.setItem(key, value);
+    if (!isMounter.current) {
+      isMounter.current = true;
+    } else {
+      console.log("useSemiPersistentState.useEffect");
+      localStorage.setItem(key, value);
+    }
   }, [value, key]);
   return [value, setValue];
 };
